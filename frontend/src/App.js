8de78fc2bb408e067ef70da96412d06310a0c260
Gaurav <rothkumar12@gmail.com>
2025-07-26 13:46:37 -0700

    fixed live stream

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
-//import mqtt from "mqtt";
+import mqtt from "mqtt";
 import "./App.css";
 
 function App() {
